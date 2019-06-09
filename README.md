0f4d086ace1027cc1742d9d703a5f2be0b1b8f9b
three4c <three4c@gmail.com>
2019-06-10 00:40:18 +0900

    とりあえずスマホ対応

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # tab-test
+Github Pages :  [https://three4c.github.io/tab-test/.](https://three4c.github.io/tab-test/)
+
+## Build Setup
+### install dependencies
+```
+npm i
+```
+
+### serve with hot reload at localhost:3000
+```
+npm run start
+```
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,18 @@
 $(function () {
-  $('.tab__item').on('click', function () {
-    let index = $('.tab__item').index(this);
-    $('.tab__item').removeClass('active');
-    $(this).addClass('active');
-    $('.content__item').removeClass('show').eq(index).addClass('show');
-    $('.content__img').removeClass('move').eq(index).addClass('move');
-    return false;
-  })
+    $('.tab__item').on('click', function () {
+        let index = $('.tab__item').index(this);
+        $('.tab__item').removeClass('active');
+        $(this).addClass('active');
+        $('.content__item').removeClass('show').eq(index).addClass('show');
+        $('.content__img').removeClass('moveLeft').eq(index).addClass('moveLeft');
+        $('.content__ruby').removeClass('slide slide-ruby').eq(index).addClass('slide slide-ruby');
+        $('.content__name').removeClass('slide slide-name').eq(index).addClass('slide slide-name');
+        $('.content__text').removeClass('slide slide-text').eq(index).addClass('slide slide-text');
+        if (index == 2) {
+            $('.content__shop').addClass('moveRight');
+        } else {
+            $('.content__shop').removeClass('moveRight')
+        }
+        return false;
+    })
 })
